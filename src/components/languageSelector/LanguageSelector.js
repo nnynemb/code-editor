614b01f07228a395626f64c47c78ed3e32f095d7
pushpendu <pushpendu.ghosh@apptware.com>
2024-11-29 23:18:59 +0530

    ui improvement

--- src/components/languageSelector/LanguageSelector.js
+++ src/components/languageSelector/LanguageSelector.js
@@ -4,19 +4,19 @@ const languages = [
     "NodeJS", "Python", "Java"
 ];
 const LanguageSelector = ({onLanguageSelect}) => {
-    const [language, selectLanguage] = useState();
+    const [language, selectLanguage] = useState("nodejs");
     useEffect(()=>{
         onLanguageSelect(language)
     },[language])
     return (
         <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                {language || 'Select language'}
+            <button className="btn btn-sm btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                {language ? `Selected language : ${language}` : 'Select language'}
             </button>
             <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
                 {
                     languages.map((l,i) => (
-                        <li key={i} onClick={()=>{selectLanguage(l.toLowerCase())}}><span className="dropdown-item cursor-pointer">{l}</span></li>
+                        <li key={i} onClick={()=>{selectLanguage(l.toLowerCase())}}><span className="btn-sm dropdown-item cursor-pointer">{l}</span></li>
                     ))
                 }
             </ul>
